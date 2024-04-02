a57b25ea11fcaa0692b774616769cee7b63d68af
fit-max <41027069+fit-max@users.noreply.github.com>
2024-04-02 12:38:39 -0600

    Update ActivationStatus.ps1

--- DomotzInstall/ActivationStatus.ps1
+++ DomotzInstall/ActivationStatus.ps1
@@ -8,9 +8,13 @@ try {
         if ($StatusResponse.mode -eq "CONFIGURED") {
             Write-Output "Mode is $($StatusResponse.mode)"
             Write-Output "Installed version is $($StatusResponse.package.pkg_version)"
+            $StatusResponse.mode | Out-File -FilePath c:\temp\domotzstatus.txt
+            $StatusResponse.package.pkg_version | Out-File -FilePath c:\temp\domotzversion.txt
         } else {
             Write-Output "Mode is $($StatusResponse.mode)"
+            $StatusResponse.mode | Out-File -FilePath c:\temp\domotzstatus.txt
         }
     } catch {
         Write-Host "No valid status from Domotz"
+        Set-Content -Path c:\temp\domotzstatus.txt -Value 'UNKNOWN'
     }
