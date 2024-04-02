6f452202ea4f58828630c8c383bbafa65ba351e8
fit-max <41027069+fit-max@users.noreply.github.com>
2024-04-02 12:29:10 -0600

    Update ActivationStatus.ps1

--- DomotzInstall/ActivationStatus.ps1
+++ DomotzInstall/ActivationStatus.ps1
@@ -1,23 +1,16 @@
 # Define the HTTP endpoint URL
-$endpointUrl = "http://127.0.0.1:3000/api/v1/status"
+$StatusUrl = "http://127.0.0.1:3000/api/v1/status"
 
-# Send the HTTP request and store the response
-$response = Invoke-RestMethod -Uri $endpointUrl -Method Get
-
-# Check if the response is successful (status code 200)
-if ($response.StatusCode -eq 200) {
-    # Parse the JSON response
-    $jsonData = $response.Content | ConvertFrom-Json
-    
-    # Check if the "mode" field is equal to "CONFIGURED"
-    if ($jsonData.mode -eq "CONFIGURED") {
-        Write-Output "Mode is CONFIGURED"
-        # You can add further actions here if needed
-    } else {
-        Write-Output "Mode is not CONFIGURED"
-        # You can add alternative actions here if needed
+try {
+        $StatusResponse = Invoke-RestMethod -Uri $StatusUrl
+        
+        # Check if the "mode" field is equal to "CONFIGURED"
+        if ($StatusResponse.mode -eq "CONFIGURED") {
+            Write-Output "Mode is $($StatusResponse.mode)"
+            Write-Output "Installed version is $($StatusResponse.package.pkg_version)"
+        } else {
+            Write-Output "Mode is $($StatusResponse.mode)"
+        }
+    } catch {
+        Write-Host "No valid status from Domotz"
     }
-} else {
-    Write-Output "Failed to fetch data from the endpoint"
-    # You can handle other status codes here if needed
-}
